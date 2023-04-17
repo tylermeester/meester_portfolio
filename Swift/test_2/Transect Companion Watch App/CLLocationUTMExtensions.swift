16db9efa989059bf949957a30276251830c4fb83
Tyler <meester600@gmail.com>
2023-04-17 16:10:16 -0700

    updates

---
+++ Swift/test_2/Transect Companion Watch App/CLLocationUTMExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  CLLocationUTMExtensions.swift
+//  Transect Companion Watch App
+//
+//  Created by Tyler Meester on 4/17/23.
+//
+
+import Foundation
---
+++ Swift/test_2/Transect Companion Watch App/Transect_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Transect_CompanionApp.swift
+//  Transect Companion Watch App
+//
+//  Created by Tyler Meester on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Transect_Companion_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
